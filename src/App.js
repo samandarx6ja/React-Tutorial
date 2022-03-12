47bc104e1861ffc26923f31e885d0b77b4e70a3d
Abdulaziz <73533363+Abdulaziz168@users.noreply.github.com>
2022-03-13 00:04:38 +0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 function App() {
+
+  const [likes, setLikes] = useState(6);
+  const [value, setValue] = useState('Input')
+  function inc(){
+    setLikes(likes + 1)
+  }
+
+  function dec(){
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <h1>Hello wolrd</h1>
+      <h1>{ likes }</h1>
+      <h1>{ value }</h1>
+      <input type='text'  onChange={e => setValue(e.target.value)} />
+      <button onClick={inc} > Increment</button>
+      <button onClick={dec} > Decrement</button>
     </div>
   );
 }
